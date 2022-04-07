b45b555332f1c9448debbd069605b8105c86af0e
furkankapici <83639181+furkankapici@users.noreply.github.com>
2022-04-07 18:10:11 +0300

    Add files via upload

---
+++ kaynaklar/soru3script.js
@@ -0,0 +1,321 @@
+function soru3satir1kalin() {
+var curPos =
+document.getElementById("3field1").selectionStart;
+console.log(curPos);
+let x = $("#3field1").val();
+let text_to_insert = $("#insert").val();
+$("#3field1").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+            }
+	   
+function soru3satir2kalin() {
+var curPos =
+document.getElementById("3field3").selectionStart;
+console.log(curPos);
+let x = $("#3field3").val();
+let text_to_insert = $("#insert").val();
+$("#3field3").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+	                }
+			
+function soru3satir3kalin() {
+var curPos =
+document.getElementById("3field5").selectionStart;
+console.log(curPos);
+let x = $("#3field5").val();
+let text_to_insert = $("#insert").val();
+$("#3field5").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+			            }
+				    
+function soru3satir4kalin() {
+var curPos =
+document.getElementById("3field7").selectionStart;
+console.log(curPos);
+let x = $("#3field7").val();
+let text_to_insert = $("#insert").val();
+$("#3field7").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+				        }
+function soru3satir5kalin() {
+var curPos =
+document.getElementById("3field9").selectionStart;
+console.log(curPos);
+let x = $("#3field9").val();
+let text_to_insert = $("#insert").val();
+$("#3field9").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos))						    
+                                        }
+			
+function soru3satir6kalin() {
+var curPos =
+document.getElementById("3field11").selectionStart;
+console.log(curPos);
+let x = $("#3field11").val();
+let text_to_insert = $("#insert").val();
+$("#3field11").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+					}
+function soru3satir7kalin() {
+var curPos =
+document.getElementById("3field13").selectionStart;
+console.log(curPos);
+let x = $("#3field13").val();
+let text_to_insert = $("#insert").val();
+$("#3field13").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+					}
+function soru3satir8kalin() {
+var curPos =
+document.getElementById("3field15").selectionStart;
+console.log(curPos);
+let x = $("#3field15").val();
+let text_to_insert = $("#insert").val();
+$("#3field15").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+				        }
+function soru3satir9kalin() {
+var curPos =
+document.getElementById("3field17").selectionStart;
+console.log(curPos);
+let x = $("#3field17").val();
+let text_to_insert = $("#insert").val();
+$("#3field17").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+					}				
+function soru3satir10kalin() {
+var curPos =
+document.getElementById("3field19").selectionStart;
+console.log(curPos);
+let x = $("#3field19").val();
+let text_to_insert = $("#insert").val();
+$("#3field19").val(
+x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+					}
+
+	function soru3ayar1() {
+   var textbox = document.getElementById('3field1');
+   var paragraph = document.getElementById('3field2');
+   paragraph.textContent = textbox.value;
+    }
+        function soru3ayar2() {
+  var textbox = document.getElementById('3field3');
+  var paragraph = document.getElementById('3field4');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar3() {
+  var textbox = document.getElementById('3field5');
+  var paragraph = document.getElementById('3field6');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar4() {
+  var textbox = document.getElementById('3field7');
+  var paragraph = document.getElementById('3field8');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar5() {
+  var textbox = document.getElementById('3field9');
+  var paragraph = document.getElementById('3field10');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar6() {
+  var textbox = document.getElementById('3field11');
+  var paragraph = document.getElementById('3field12');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar7() {
+  var textbox = document.getElementById('3field13');
+  var paragraph = document.getElementById('3field14');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar8() {
+  var textbox = document.getElementById('3field15');
+  var paragraph = document.getElementById('3field16');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar9() {
+  var textbox = document.getElementById('3field17');
+  var paragraph = document.getElementById('3field18');
+  paragraph.textContent = textbox.value;
+}
+        function soru3ayar10() {
+  var textbox = document.getElementById('3field19');
+  var paragraph = document.getElementById('3field20');
+  paragraph.textContent = textbox.value;
+}   
+     function soru3sıkA() {
+var textbox = document.getElementById('3field21');
+var paragraph = document.getElementById('3field22');
+paragraph.textContent = textbox.value;
+ }
+     function soru3sıkB() {
+var textbox = document.getElementById('3field23');
+var paragraph = document.getElementById('3field24');
+paragraph.textContent = textbox.value;
+ }
+     function soru3sıkC() {
+var textbox = document.getElementById('3field25');
+var paragraph = document.getElementById('3field26');
+paragraph.textContent = textbox.value;
+ }
+     function soru3sıkD() {
+var textbox = document.getElementById('3field27');
+var paragraph = document.getElementById('3field28');
+paragraph.textContent = textbox.value;
+ }
+ 
+ 
+     function soru3dogrusıkA() {
+var textbox = document.getElementById('3field21');
+var paragraph = document.getElementById('3field29');
+paragraph.textContent = textbox.value;
+ }
+     function soru3dogrusıkB() {
+var textbox = document.getElementById('3field23');
+var paragraph = document.getElementById('3field29');
+paragraph.textContent = textbox.value;
+ }
+     function soru3dogrusıkC() {
+var textbox = document.getElementById('3field25');
+var paragraph = document.getElementById('3field29');
+paragraph.textContent = textbox.value;
+ }
+     function soru3dogrusıkD() {
+var textbox = document.getElementById('3field27');
+var paragraph = document.getElementById('3field29');
+paragraph.textContent = textbox.value;
+ }
+ 
+ function soru3satir1cizili() {
+ var curPos =
+ document.getElementById("3field1").selectionStart;
+ console.log(curPos);
+ let x = $("#3field1").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field1").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+             }
+	   
+ function soru3satir2cizili() {
+ var curPos =
+ document.getElementById("3field3").selectionStart;
+ console.log(curPos);
+ let x = $("#3field3").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field3").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+ 	                }
+			
+ function soru3satir3cizili() {
+ var curPos =
+ document.getElementById("3field5").selectionStart;
+ console.log(curPos);
+ let x = $("#3field5").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field5").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+ 			            }
+				    
+ function soru3satir4cizili() {
+ var curPos =
+ document.getElementById("3field7").selectionStart;
+ console.log(curPos);
+ let x = $("#3field7").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field7").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+ 				        }
+ function soru3satir5cizili() {
+ var curPos =
+ document.getElementById("3field9").selectionStart;
+ console.log(curPos);
+ let x = $("#3field9").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field9").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos))						    
+                                         }
+			
+ function soru3satir6cizili() {
+ var curPos =
+ document.getElementById("3field11").selectionStart;
+ console.log(curPos);
+ let x = $("#3field11").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field11").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+ 					}
+ function soru3satir7cizili() {
+ var curPos =
+ document.getElementById("3field13").selectionStart;
+ console.log(curPos);
+ let x = $("#3field13").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field13").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+ 					}
+ function soru3satir8cizili() {
+ var curPos =
+ document.getElementById("3field15").selectionStart;
+ console.log(curPos);
+ let x = $("#3field15").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field15").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+ 				        }
+ function soru3satir9cizili() {
+ var curPos =
+ document.getElementById("3field17").selectionStart;
+ console.log(curPos);
+ let x = $("#3field17").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field17").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+ 					}
+					
+ function soru3satir10cizili() {
+ var curPos =
+ document.getElementById("3field19").selectionStart;
+ console.log(curPos);
+ let x = $("#3field19").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field19").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));				
+                                        }
+ function soru3Asikcizili() {
+ var curPos =
+ document.getElementById("3field21").selectionStart;
+ console.log(curPos);
+ let x = $("#3field21").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field21").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+                                        }					
+ function soru3Bsikcizili() {
+ var curPos =
+ document.getElementById("3field23").selectionStart;
+ console.log(curPos);
+ let x = $("#3field23").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field23").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+				        }
+									       
+ function soru3Csikcizili() {
+ var curPos =
+ document.getElementById("3field25").selectionStart;
+ console.log(curPos);
+ let x = $("#3field25").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field25").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+				        }
+														      
+ function soru3Dsikcizili() {
+ var curPos =
+ document.getElementById("3field27").selectionStart;
+ console.log(curPos);
+ let x = $("#3field27").val();
+ let text_to_insert = $("#insert2").val();
+ $("#3field27").val(
+ x.slice(0, curPos) + text_to_insert + x.slice(curPos));
+			                }
