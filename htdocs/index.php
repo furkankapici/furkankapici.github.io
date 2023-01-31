8810af807c41905f913f82fd5bf3f5842dd73584
furkankapici <83639181+furkankapici@users.noreply.github.com>
2023-01-31 12:23:27 +0300

    Add files via upload
    Sorunlar giderildi

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,111 +1,78 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-  <head>
-  <script>
-  document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  // Gönderilen mesajın işlemleri
-});
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    // Gönderilen mesajın işlemleri
+<head>
+<style>
+#chatContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0 auto;
+  position: absolute;
+}
+
+#messages {
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  border: 1px solid black;
+}
+
+#input {
+  width: 80%;
+  height: 20px;
+  margin-bottom: 10px;
+}
+
+#submit {
+  width: 20%;
+  height: 30px;
+}
+
+#parent {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
+    <script>
+window.onload = function() {
+  const chatContainer = document.getElementById("chatContainer");
+  const messages = document.getElementById("messages");
+  messages.scrollTop = messages.scrollHeight;
+  const input = document.getElementById("input");
+  const submit = document.getElementById("submit");
+
+  input.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+      const message = input.value;
+      messages.innerHTML += "<br>" + message;
+      input.value = "";
+    }
   });
+  
+submit.addEventListener("click", function() {
+const message = input.value;
+const newMessage = document.createElement("div");
+newMessage.innerText = message;
+messages.appendChild(newMessage);
+input.value = "";
+messages.scrollTop = messages.scrollHeight;
 });
+};
 
-  </script>
-    <style>
-	  form {
-       position: fixed;
-       bottom: 0;
-       width: 100%;
-       background-color: white;
-      }
-
-      textarea {
-       width: 100%;
-       height: 100px;
-      }
-
-      .chat-container {
-        width: 80%;
-        margin: auto;
-        padding: 20px;
-        background-color: #f2f2f2;
-        border-radius: 10px;
-        box-shadow: 2px 2px 5px #888888;
-      }
-      .chat-header {
-        text-align: center;
-        font-size: 22px;
-        font-weight: bold;
-        margin-bottom: 20px;
-      }
-      .chat-message {
-        margin-bottom: 20px;
-        padding: 10px;
-        background-color: #ffffff;
-        border-radius: 10px;
-        box-shadow: 1px 1px 2px #888888;
-      }
-      .message-header {
-        font-size: 18px;
-        font-weight: bold;
-        margin-bottom: 10px;
-      }
-      .message-body {
-        font-size: 16px;
-      }
-	    form {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: white;
-    padding: 20px;
-    width: 100%;
-    border-top: 1px solid #f2f2f2;
-  }
-
-  textarea {
-    width: 80%;
-    height: 100px;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #f2f2f2;
-    border-radius: 10px;
-    resize: none;
-  }
-
-  button[type="submit"] {
-    width: 20%;
-    height: 100px;
-    background-color: #f2f2f2;
-    color: black;
-    border: none;
-    border-radius: 10px;
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer;
-  }
-    </style>
-  </head>
-  <body>
-    <div class="chat-container">
-      <div class="chat-header">Sohbet Başlığı</div>
-      <div class="chat-message">
-        <div class="message-header">Kullanıcı Adı</div>
-        <div class="message-body">Mesaj İçeriği</div>
-      </div>
-      <div class="chat-message">
-        <div class="message-header">Kullanıcı Adı</div>
-        <div class="message-body">Mesaj İçeriği</div>
-      </div>
-	  <div class="chat-message">
-<form>
-  <textarea></textarea>
-  <button type="submit">Gönder</button>
-</form>
+    </script>
+</head>
+<body>
+<div id="chatContainer">
+  <div id="messages"><br>ew<br><br>trfhddh<br>kc<br>h<br>fk<br>j<br>f<br>h<br>j<br>g<br>hk<br>ghk<br>y<br>tr<br>t<br>r<br>t<br>rt<br>rt<br>r<br>t<br>rt<br><br><br>r<br>t<br>rt<br><br>ret<br>et<br>r<br>e<br>tt<br>rt<br>rterrtiş<br>üerşydhptrkmyjptpxdkğ<br>jkyg<br>tcyğ<br>gklulğ<br>yulğ<br>ul<br>yulğ<br>yulo<br>ğyu<br>ayak yala himor</div>
+  <input type="text" id="input">
+  <button id="submit">Gönder</button>
 </div>
-    </div>
-  </body>
+</body>
 </html>
