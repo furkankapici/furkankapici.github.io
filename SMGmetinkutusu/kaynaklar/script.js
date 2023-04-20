1b2e894b23b8e3ac2aa6620331e6c8fd581e9319
furkankapici <83639181+furkankapici@users.noreply.github.com>
2023-04-20 12:58:26 +0300

    Update script.js

--- SMGmetinkutusu/kaynaklar/script.js
+++ SMGmetinkutusu/kaynaklar/script.js
@@ -16,29 +16,38 @@ return false;
 
 function satirsiyah() {
 var curPos =
-document.getElementById("field1").selectionStart;
+document.getElementById("htmlmetin").selectionStart;
 console.log(curPos);
-let x = $("#field1").val();
+let x = $("#htmlmetin").val();
 let text_to_insert = $("#insert").val();
-$("#field1").val(
+$("#htmlmetin").val(
 x.slice(0, curPos) + text_to_insert + x.slice(curPos));
             }
 
  function satirkirmizi() {
  var curPos =
- document.getElementById("field1").selectionStart;
+ document.getElementById("htmlmetin").selectionStart;
  console.log(curPos);
- let x = $("#field1").val();
+ let x = $("#htmlmetin").val();
  let text_to_insert = $("#insert2").val();
- $("#field1").val(
+ $("#htmlmetin").val(
  x.slice(0, curPos) + text_to_insert + x.slice(curPos));
              }	  
 
 	function satirayar() {
-   var textbox = document.getElementById('field1');
-   var paragraph = document.getElementById('field2');
-   paragraph.textContent = textbox.value;
-    }
- 
+var metinKutusu = document.getElementById("htmlmetin"); // metin kutusu elemanını al
+var sonucKutusu = document.getElementById("sorujs"); // sonuç kutusu elemanını al
+var htmlKod = metinKutusu.value; // metin kutusundaki HTML kodunu al
+sonucKutusu.innerHTML = htmlKod; // HTML kodunu işle ve sonuç kutusuna yaz
+}
 
-	   
+function donustur() {
+  // Metin kutusundan metni al
+  var metin = document.getElementById("htmlmetin").value;
+
+  // <a> etiketlerini <font> etiketlerine dönüştür
+  var yeniMetin = metin.replace(/<a style='color: red;'>/g, '<font color="red"/>').replace(/<\/a>/g, "").replace(/<a style='color: black;'>/g, '<font color="black"/>').replace(/<\/a>/g, "");
+
+  // Sonucu yeni bir metin kutusuna yaz
+  document.getElementById("sonucKutusu").value = yeniMetin;
+}
